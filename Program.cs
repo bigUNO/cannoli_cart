9ae9209d40f10b6c7c9b5df29f236bd0a018189f
LeonBH <leon.johnson@bhemail.com>
2018-02-28 10:01:58 -0500

    :sparkles: Returns all the values
    Not sure if this is how it should work

--- Program.cs
+++ Program.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.IO;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 public class RecursiveFileProcessor 
 {
-    public static void Main(string[] args)
-    {
-        ProcessDirectory("data");
-          
-    }
+    public static void Main(string[] args) => ProcessDirectory("data");
 
-    // Process all files in the directory passed in, recurse on any directories 
-    // that are found, and process the files they contain.
     public static void ProcessDirectory(string targetDirectory) 
     {
         // Process the list of files found in the directory.
@@ -22,7 +17,20 @@ public class RecursiveFileProcessor
 
     public static void ProcessFile(string file)
     {
-        Console.WriteLine("    Processing lines in {0}", file);
+        List<string> parsedData = new List<string>();
+        //Console.WriteLine("    Processing lines in {0}", file);
+
+        using (StreamReader readFile = new StreamReader(file))
+        {
+            string line;
+            string[] row;
+
+            while ((line = readFile.ReadLine()) != null)
+            {
+                row = line.Split(',');
+                Console.WriteLine("Value: {0}",row.GetValue(0));
+            } 
+        }
     }
 
     public static void SelectFile(string path) 
